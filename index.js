41caae5c2ec8223d086517a16bed7cbf82e09665
Andrew <mitch702@msu.edu>
2017-03-11 02:00:39 -0500

    adding comments

--- index.js
+++ index.js
@@ -1,71 +1,95 @@
-function mainRound(){
-  var play = window.confirm("In this game you pick if the next number is going to be higher or lower, based on the number given.  Numbers will be Integers from 1 to 100.  If you want to play press OK.")
-  var number = Math.round(Math.random() * 100)
-  var newNumber = Math.round(Math.random() * 100)
-  var correct = 3
-
-  if (play){
+function mainRound () {
+  //ask player if they want to play
+  var play = window.confirm('In this game you pick if the next number is going to be higher or lower, based on the number given.  Numbers will be Integers from 1 to 100.  If you want to play press OK.')
+  if (play) {
+    //intialze some variables for game
+    var number = Math.round(Math.random() * 100)
+    var newNumber = Math.round(Math.random() * 100)
+    var correct = 3
+    // continue game until told to quit with cancel
     while (true) {
+      //ask for higher or lower
       var input = window.prompt('The current number is ' + number + '.  Is the next number higer or lower? (Press Cancel to quit)')
+      //player hit cancel
       if (input === null) {
-          break
+        break
+      //lower case
       } else if (input.toLowerCase().trim() === 'lower') {
         if (newNumber < number) {
           correct -= 1
           window.alert('You are Correct! (' + (correct) + ' more correct to win)')
         } else {
-          window.alert("You are Wrong!")
+          window.alert('You are Wrong!')
         }
+      //higher case
       } else if (input.toLowerCase().trim() === 'higher') {
         if (newNumber > number) {
           correct -= 1
           window.alert('You are Correct! (' + (correct) + ' more correct to win)')
         } else {
-          window.alert("You are Wrong!")
+          window.alert('You are Wrong!')
         }
+      //otherwise it is invalid
       } else {
-        window.alert("That is not a valid input!")
+        window.alert('That is not a valid input!')
       }
-      if (correct === 0){
-        if (bonusRound()){
-          window.alert("YOU WON THE BONUS ROUND")
-        } else {
-          window.alert("YOU LOST THE BONUS ROUND")
+      //if we are done with the game
+      if (correct === 0) {
+        //do a bonus round
+        if (bonusRound()) { //player won the bonus round
+          window.alert('YOU WON THE BONUS ROUND')
+        } else { //player lost the bonus round
+          window.alert('YOU LOST THE BONUS ROUND')
         }
+        //we are done with the game so break
         break
       }
+      //set the new number to the main number and grab a new secret number
       number = newNumber
       newNumber = Math.round(Math.random() * 100)
     }
-  }
-  else{
-    window.alert("bummer")
+  //player did not want to play the game
+  } else {
+    window.alert('bummer')
   }
 }
 
-function bonusRound(){
-  var playBonus = window.confirm("This is the Bonus Round! Press OK to play or Cancel to quit")
-  if (playBonus) {
-    while(true){
+function bonusRound () {
+  //ask player if they want to play bonus round
+  var playBonus = window.confirm('This is the Bonus Round! Press OK to play or Cancel to quit')
+  if (playBonus) { //they do
+    //keep repeating till told otherwise
+    while (true) {
+      //pick random number
       var oneNumber = Math.round(Math.random() * 10)
-      input = window.prompt("Pick a number between 0 and 10")
+      //ask them to guess that random number
+      var input = window.prompt('Pick a number between 0 and 10')
+      //if they hit cancel
       if (input === null) {
         break
-      } else if (isNaN(input)){
-        window.alert("Not a correct input!")
-      }else{
-        if (parseInt(input) === oneNumber){
+      //if they did not enter a number
+      } else if (isNaN(input)) {
+        window.alert('Not a correct input!')
+      //they did enter a number
+      } else {
+        //they get the correct number
+        if (parseInt(input) === oneNumber) {
           return true
-        } else if (parseInt(input) > 10 || parseInt(input) < 0){
-          window.alert("The number has to be between 0 and 10")
+        //the picked a number outside the range
+        } else if (parseInt(input) > 10 || parseInt(input) < 0) {
+          window.alert('The number has to be between 0 and 10')
+        //they picked the wrong number
         } else {
           return false
         }
       }
     }
+
+  //they did not want to play the game
   } else {
-    window.alert("No Bonus Round!")
+    window.alert('No Bonus Round!')
   }
 }
 
+//call the main game
 mainRound()
